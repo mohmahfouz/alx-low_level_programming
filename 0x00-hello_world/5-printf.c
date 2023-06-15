84a3cdb847706868553f2b52070a29576d78735e
mohmahfouz <7watki@gmail.com>
2023-06-15 16:42:57 -0700

    new c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("with proper grammar, but the outcome is a piece of art.");
-  return 0;
+/**
+ * main - Entry point
+ *
+ * Description: using printf print 'with proper grammer....'
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
