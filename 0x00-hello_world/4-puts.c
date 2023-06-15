44c03142c318546ff58e6b1fbc9a40948d6ae270
mohmahfouz <7watki@gmail.com>
2023-06-15 15:48:04 -0700

    new c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
+/**
+ * main function: a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * return 0: the program should end with the value 0
+ */
+
 #include <stdio.h>
 
-int main() {
-  puts("Programming is like building a multilingual puzzle\n");
-  return 0;
+int main(void) 
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
 }
