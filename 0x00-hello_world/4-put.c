2d59fdb6b27fe2878446749d8af9e7d67fa8c05d
mohmahfouz <7watki@gmail.com>
2023-06-15 15:34:26 -0700

    new c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,11 @@
+/**
+ * main: Main function
+ *
+ * Description: a C program that prints exactly "Programming is like building a multilingual puzzle"
+ *
+ * return 0 
+ */
+int main(void) {
+  puts("Programming is like building a multilingual puzzle\n");
+  return 0;
+}
