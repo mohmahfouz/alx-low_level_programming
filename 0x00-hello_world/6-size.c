d5d30420e87261c763a68232cdcd8502976c6c21
mohmahfouz <7watki@gmail.com>
2023-06-15 16:53:35 -0700

    new c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * Main - entry point
+ *
+ * Desctiption - a C program that prints the size of various types on the computer it is compiled and run on.
+ * 
+ * Return: 0 
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lii;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lii));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
