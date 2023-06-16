a156092dc1517e9ac44f7995365312ff11b435c8
mohmahfouz <7watki@gmail.com>
2023-06-15 17:29:02 -0700

    new c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-fputs("and that piece of art is useful\n- Dora Korpar, 2015-10-19", stderr);
+fputs("and that piece of art is useful Dora Korpar, 2015-10-19", stderr);
 return (1);
 }
