fc9840ed6ca3776559115b63865a490860e6555d
mohmahfouz <7watki@gmail.com>
2023-06-15 17:25:24 -0700

    new c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints quote
+*
+* Description : prints dora korpar quote
+*
+* Return: 1
+**/
+
+int main(void)
+{
+fputs("and that piece of art is useful\n- Dora Korpar, 2015-10-19", stderr);
+return (1);
+}
