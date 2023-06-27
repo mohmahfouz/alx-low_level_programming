6aeea77cae2c869b0133b1eb86ec886c0d3cdecf
mohmahfouz <7watki@gmail.com>
2023-06-27 07:37:52 -0700

    jun27

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies source to destination
+ * @sourc: source
+ * @destination: destination
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *destination, char *source)
+{
+int i = 0;
+
+while (*(source + i))
+{
+*(destination + i) = *(source + i);
+i++;
+}
+*(destination + i) = '\0';
+return (destination);
+}
