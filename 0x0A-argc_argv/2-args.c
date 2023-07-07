628ffe4da8d9c55da166f6c7aa0f7b311e4ffe55
mohmahfouz <7watki@gmail.com>
2023-07-07 10:27:30 -0700

    0707

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments 
+ * @argc: argument count 
+ * @argv: argument vector
+ * Return: 0
+ */
+
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
