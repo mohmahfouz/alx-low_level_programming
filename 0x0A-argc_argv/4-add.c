832d5161ece72afa86de8271a14009a446da39f2
mohmahfouz <7watki@gmail.com>
2023-07-07 11:02:15 -0700

    0707

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - positive number addition
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 1 if symbol
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b, addition = 0;
+
+for (a = 1; a < argc; a++)
+{
+for (b = 0; argv[b][b] != '\0'; b++)
+{
+if (!isdigit(argv[a][b]))
+{
+printf("Error\n");
+return (1);
+}
+}
+addition += atoi(argv[a]);
+}
+printf("%d\n", addition);
+return (0);
+}
