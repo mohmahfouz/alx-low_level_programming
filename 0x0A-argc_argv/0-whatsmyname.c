6d75c99d2fd61185837665f07c2938907131403d
mohmahfouz <7watki@gmail.com>
2023-07-07 10:00:23 -0700

    0707

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print app name then new line
+ * @argc: counter
+ * @argv: vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
