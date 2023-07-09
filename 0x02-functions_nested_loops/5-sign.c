611c1e93f1dfdd466b95cdc7c5aacbb51744ae04
mohmahfouz <7watki@gmail.com>
2023-07-09 08:11:56 -0700

    0807

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - entry
+* @c: character
+* Return: 1 in lower case
+*/
+int print_sign(int c)
+{
+if (c > 0)
+{
+_putchar('+');
+	return (1);
+}
+else if (c == 0)
+{
+_putchar('0');
+return (0);
+	}
+else if (c < 0)
+{
+_putchar('-');
+return (-1);
+}
+return (0);
+}
