8bb7b4c1ca3d23a4252863b3d7fb80f2c4faf6e3
mohmahfouz <7watki@gmail.com>
2023-07-09 04:02:27 -0700

    0807

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - reverse string
+ * @s: string 
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(s[0]);
+}
+}
