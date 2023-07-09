e4fa0f461ae7217b03ca861ac0566fad9d0f51b3
mohmahfouz <7watki@gmail.com>
2023-07-09 05:24:48 -0700

    0807

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+/**
+ * wildcmp - Compare two strings
+ * @s1: String 1
+ * @s2: String 2
+ * Return: 1 if considered identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+return (1);
+}
+return (0);
+}
