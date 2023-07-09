969fe9a7a60cbd4ffe0648d2e28587a5c2cdb651
mohmahfouz <7watki@gmail.com>
2023-07-09 04:29:29 -0700

    0807

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
+/**
+ * find_sqrt - find square root
+ * @num: The number
+ * @root: The root
+ * Return: square root if applicaple
+ *         -1 if not applicable
+ */
+int find_sqrt(int num, int root)
+{
+if ((root * root) == num)
+return (root);
+if (root == num / 2)
+return (-1);
+return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns square root 
+ * @n: The number
+ *
+ * Return: square root if applicable
+ *         -1 if not applicable
+ */
+int _sqrt_recursion(int n)
+{
+int root = 0;
+if (n < 0)
+return (-1);
+if (n == 1)
+return (1);
+return (find_sqrt(n, root));
+}
