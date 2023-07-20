3493b2d2f2ccb69e4012359a1ad390342387b0e0
mohmahfouz <7watki@gmail.com>
2023-07-20 10:07:44 -0700

    m

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - check code
+* Return: nothing
+*/
+int main(int ac, char *av[])
+{
+char *s;
+
+s = argstostr(ac, av);
+if (s == NULL)
+{
+return (1);
+}
+printf("%s", s);
+free(s);
+return (0);
+}
