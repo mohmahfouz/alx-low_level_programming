5fbdb64e3b77823005d09d2ce4e0f82f591b80a0
mohmahfouz <7watki@gmail.com>
2023-07-20 08:55:12 -0700

    2

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,40 +1,32 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
-* get_op_func - Retrieves function
-* @s: pointer
-* Return: Pointer
+* get_op_func - get_op_func
+* @s: string
+*
+* Return:nothing
 */
-
 int (*get_op_func(char *s))(int, int)
 {
-
 op_t ops[] = {
-
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
+{ "+", op_add },
+{ "-", op_sub },
+{ "*", op_mul },
+{ "/", op_div },
+{ "%", op_mod },
+{ NULL, NULL }
 };
-
-
 int i = 0;
 
-
-
-/* while 1< 4, s and s[0] (args) == pos. in struct.s[1] is empt =? true */
-
-while (i <= 4)
+while (i < 5)
 {
-
-if (s && s[0] == ops[i].op[0] && !s[1])
-{
+if (strcmp(s, ops[i].op) == 0)
 return (ops[i].f);
-}
 
 i++;
 }
-return (NULL);
+
+return (0);
 }
