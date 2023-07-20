8044461464c8c8920c33496083cba8266fe950f4
mohmahfouz <7watki@gmail.com>
2023-07-20 08:43:56 -0700

    2

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,14 +2,14 @@
 
 /**
 * op_add - adds
-* @a: number 
+* @a: number
 * @b: second number
 * Return: result
 */
 
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 
 
@@ -23,7 +23,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 
 
@@ -36,7 +36,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 
 
@@ -51,7 +51,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return (a / b);
+return (a / b);
 }
 
 
@@ -65,5 +65,5 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 
-	return (a % b);
+return (a % b);
 }
