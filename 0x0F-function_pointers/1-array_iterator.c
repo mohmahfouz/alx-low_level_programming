7b10b1e2abeeef86beab3df819070473b9dca61d
mohmahfouz <7watki@gmail.com>
2023-07-20 05:21:37 -0700

    2007

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+* array_iterator - array__interator
+* @array: pointer
+* @size: array
+* @action: pointer
+* Return:nothing 
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned long int i;
+
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+action(array[i]);
+}
