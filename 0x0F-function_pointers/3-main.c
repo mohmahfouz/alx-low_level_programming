a7eb8baf5a93850784bcbffd93c289d6c017dae6
mohmahfouz <7watki@gmail.com>
2023-07-20 08:38:39 -0700

    2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,41 +3,38 @@
 #include <stdlib.h>
 
 /**
- * main - check the code
- * @argc: count.
-* @argv: vector.
+* main - Prints result
 *
-* Return: Always 0.
+* @argc: commandline
+*
+* @argv: commandline args
+*
+* Return: 0 success
 */
-int main(int argc, char *argv[])
-{
-int a, b;
 
-int (*operation)(int, int);
+int main(int argc, char **argv)
+{
+int (*operator_function)(int, int), num1, num2;
 
 if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+printf("Error\n"), exit(98);
 
-if (argv[2][1])
-{
-printf("Error\n");
-exit(99);
-}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-operation = get_op_func(argv[2]);
-
-if (operation == NULL)
+operator_function = get_op_func(argv[2]);
+if (!operator_function)
 {
 printf("Error\n");
 exit(99);
 }
 
-a = atoi(argv[1]);
-b = atoi(argv[3]);
+if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
+{
+printf("Error\n");
+exit(100);
+}
 
-printf("%d\n", operation(a, b));
+printf("%d\n", operator_function(num1, num2));
 return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,73 +1,69 @@
 #include "3-calc.h"
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * op_add - adds
- * @a: first
- * @b: second
- *
- * Return: result
- */
+* op_add - adds
+* @a: number 
+* @b: second number
+* Return: result
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+
+
 /**
- * op_sub - subctracts
- * @a: first
- * @b: second
- *
- * Return:result
- */
+* op_sub - subs
+* @a: number
+* @b: second number
+* Return: result
+*/
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+
 /**
- * op_mul - multiplies
- * @a: first
- * @b: second
- *
- * Return: result.
- */
+* op_mul - multiply
+* @a: number
+* @b: second number
+* Return: result
+*/
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-/**
- * op_div - divides
- * @a: number.
- * @b: number.
- *
- * Return: result.
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-		return (a / b);
-}
+
 
 /**
- * op_mod - module of two numbers
- * @a: first
- * @b: second
- *
- * Return: remainder
- */
+* op_div - division
+* @a: numberk
+* @b: second number
+* Return: result
+*/
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+* op_mod - modulus
+* @a: number
+* @b: second number
+* Return: result
+*/
+
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+
 	return (a % b);
 }
