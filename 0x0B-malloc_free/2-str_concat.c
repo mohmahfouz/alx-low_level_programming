0c401f47a43529684fb92690a6094668cf732053
mohmahfouz <7watki@gmail.com>
2023-07-20 09:51:38 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,38 @@
-#include "main.h"
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
-* str_concat - concat
-* @s1: string
-* @s2: string 2
+* str_concat - concatenates
+* @s1: first string
+* @s2: second string
 *
-* Description: concatenate
-* Return: result
+* Return: newly allocated portion of memory containing s1 and s2
 */
-
 char *str_concat(char *s1, char *s2)
 {
-char *ptr;
-int i = 0;
-int j = 0;
+char *concat;
+int s1_len = 0, s2_len = 0;
+int i, j, size;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (s1[i])
-i++;
-while (s2[j])
-j++;
-/*IGNORE_STYLE_CHECK*/
-ptr = malloc(1 + (sizeof(char) * i) + (sizeof(char) * j));
-if (ptr == NULL)
+
+while (s1[s1_len] != '\0')
+s1_len++;
+while (s2[s2_len] != '\0')
+s2_len++;
+
+size = s1_len + s2_len;
+concat = malloc(sizeof(char) * (size + 1));
+if (concat == NULL)
 return (NULL);
-i = 0;
-while (s1[i])
-{
-ptr[i] = s1[i];
-i++;
-}
-j = 0;
-while (s2[j])
-{
-ptr[i] = s2[j];
-i++;
-j++;
-}
-ptr[i] = s2[j];
-return (ptr);
+for (i = 0; i < s1_len; i++)
+concat[i] = s1[i];
+for (j = 0; i < size && j < s2_len; i++, j++)
+concat[i] = s2[j];
+concat[i] = '\0';
+
+return (concat);
 }
